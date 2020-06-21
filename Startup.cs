d5ab53c5cbfa50c990799df44bce6c7eaa61ad1c
Rashik Tuladhar <rashiktuladhar2016@outlook.com>
2020-06-21 13:26:13 +0545

    added nuget package and implemented swagger

--- Startup.cs
+++ Startup.cs
@@ -1,16 +1,10 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace ImplementingSwagger
 {
@@ -27,6 +21,28 @@ namespace ImplementingSwagger
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("V1", new OpenApiInfo
+                {
+                    Title = "Swagger Implementation",
+                    Version = "V1",
+                    Description = "Api Description",
+                    TermsOfService = new Uri("https://rashik.com.np"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Company Name",
+                        Email = "info@email.com",
+                        Url = new Uri("https://rashik.com.np"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Swagger Implementation License",
+                        Url = new Uri("https://rashik.com.np"),
+                    }
+                });
+
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,6 +52,18 @@ namespace ImplementingSwagger
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/V1/swagger.json", "Integrating Swagger");
+                //To serve the Swagger UI at the app's root (http://localhost:<port>/), set the RoutePrefix property to an empty string:
+                //c.RoutePrefix = string.Empty;
+            });
 
             app.UseHttpsRedirection();
 
